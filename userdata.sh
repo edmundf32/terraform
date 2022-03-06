8b9b61613cb8b8772a0df72178d4a0219b968076
edmundf32 <edmundf32@gmail.com>
2022-03-06 13:59:34 +0000

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -v
-apt-get update -y
-apt-get install -y sudo apt install apache2
+sudo apt-get update -y
+sudo apt-get install -y 
+sudo apt install apache2 -y
 sudo chmod 777 /var/www/html/index.html
 sudo echo "<html><h1>Production</h1></html>" > /var/www/html/index.html

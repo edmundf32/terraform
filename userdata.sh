79ff4a9a9cbb01b4747fda58db97d85e6688d456
edmundf32 <edmundf32@gmail.com>
2022-03-06 13:43:37 +0000

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -v
 apt-get update -y
-apt-get install -y nginx > /tmp/nginx.log
+apt-get install -y sudo apt install apache2
+sudo chmod 777 /var/www/html/index.html
+sudo echo "<html><h1>Production</h1></html>" > /var/www/html/index.html
